f5f15ad81328fd17dbeae57599eef7e6629da070
Artsiom Koptsiukh <sid93.ramsey@gmail.com>
2021-01-25 13:08:11 +0300

    Initial commit

---
+++ src/main/java/ShareLane.java
@@ -0,0 +1,11 @@
+import org.testng.annotations.Test;
+import static com.codeborne.selenide.Selenide.open;
+
+public class ShareLane {
+
+    @Test
+    public static void Test1(){
+        //init test
+    }
+
+}
