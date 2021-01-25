4f622774766fcbf22840c6e13cd1cc4de6e1f2c9
Artsiom Koptsiukh <sid93.ramsey@gmail.com>
2021-01-25 18:59:21 +0300

    First test are added

--- src/main/java/ShareLane.java
+++ src/main/java/ShareLane.java
@@ -1,11 +1,38 @@
+import org.openqa.selenium.By;
+import org.testng.Assert;
 import org.testng.annotations.Test;
+
+import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
 public class ShareLane {
+    private static final String URL = "https://www.sharelane.com/";
+    private static final String REGISTER_URL = "https://www.sharelane.com/cgi-bin/register.py";
 
     @Test
-    public static void Test1(){
-        //init test
+    public static void enterSite(){
+        open(URL);
+        $(By.xpath("/html/body/center/table/tbody/tr[20]/td/p/a/b")).click();
+        //String result = $(By.className("footer")).getText();
+        String result = $(By.linkText("Shopping Cart")).getText();
+        Assert.assertEquals(result, "Shopping Cart");
+    }
+
+    @Test
+    public static void zipCodeEmpty(){
+        open("https://www.sharelane.com/cgi-bin/register.py");
+        $(By.xpath("/html/body/center/table/tbody/tr[5]/td/table/tbody/tr[2]/td/table/tbody/tr[3]/td[2]/input")).click();
+        String result = $(By.className("error_message")).getText();
+        Assert.assertEquals(result, "Oops, error on page. ZIP code should have 5 digits");
+    }
+
+    @Test
+    public static void zipCodeFiveDigits(){
+        open("https://www.sharelane.com/cgi-bin/register.py");
+        $(By.name("zip_code")).sendKeys("11111");
+        $(By.xpath("/html/body/center/table/tbody/tr[5]/td/table/tbody/tr[2]/td/table/tbody/tr[3]/td[2]/input")).click();
+        String result = $(By.xpath("/html/body/center/table/tbody/tr[5]/td/table/tbody/tr[2]/td/table/tbody/tr[1]/td[1]/p")).getText();
+        Assert.assertEquals(result, "First Name*");
     }
 
 }
